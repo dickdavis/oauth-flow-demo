bca10830d91a90a427f6756f84f51ef6b7d62f32
Dick Davis <dick@hey.com>
2023-08-11 15:17:31 -0500

    Add user model

---
+++ db/migrate/20230811194906_create_users.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+##
+# Creates the user model
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |table|
+      table.string :first_name
+      table.string :last_name
+      table.string :email, index: { unique: true }
+      table.string :password_digest
+
+      table.timestamps
+    end
+  end
+end
