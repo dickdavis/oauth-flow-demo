7913bcccf5f617b142eadbddf6d6dab393887d6f
Dick Davis <dick@hey.com>
2023-08-14 09:58:02 -0500

    Add login page

---
+++ spec/views/sessions/new.html.tailwindcss_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'sessions/new' do
+  it 'renders log in form' do
+    render
+
+    assert_select 'form[action=?][method=?]', sessions_path, 'post' do
+      assert_select 'input[name=?]', 'email'
+      assert_select 'input[name=?]', 'password'
+    end
+  end
+end
