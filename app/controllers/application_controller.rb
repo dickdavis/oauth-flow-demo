7d1c4ce90ff9b8525f6391cf0eeb524be1b0f473
Dick Davis <dick@hey.com>
2023-08-15 12:49:25 -0500

    Conditionally render home page buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+##
+# Base application controller.
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    return nil unless (id = session[:user_id])
+
+    @current_user ||= User.find(id)
+  end
 end
