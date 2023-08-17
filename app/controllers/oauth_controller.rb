5699604dc7e1d8d3ac571870a09f1ce90101aa77
Dick Davis <dick@hey.com>
2023-08-17 11:43:04 -0500

    Add client configuration with HTTP Basic auth

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,6 +3,8 @@
 ##
 # Controller for OAuth flow.
 class OAuthController < ApplicationController
+  before_action :authenticate_client
+
   # rubocop:disable Metrics/MethodLength
   def authorize
     status, body = StateTokenEncoderService.call(
@@ -21,4 +23,18 @@ class OAuthController < ApplicationController
     end
   end
   # rubocop:enable Metrics/MethodLength
+
+  private
+
+  def authenticate_client
+    return if http_basic_auth_successful?
+
+    request_http_basic_authentication
+  end
+
+  def http_basic_auth_successful?
+    authenticate_with_http_basic do |id, secret|
+      id == params[:client_id] && secret == Rails.application.credentials.clients[id.to_sym]
+    end
+  end
 end
--- spec/requests/oauth_controller_spec.rb
+++ spec/requests/oauth_controller_spec.rb
@@ -2,16 +2,64 @@
 
 require 'rails_helper'
 
-RSpec.describe OAuthController do # rubocop:disable RSpec/FilePath
-  describe 'GET /authorize' do
-    subject(:call_endpoint) { get authorize_path }
+RSpec.shared_context 'with an authenticated client' do |method, path, options = {}|
+  subject(:call_endpoint) { send(method, url, **options_for_request) }
 
+  let(:url) { send(path) }
+  let(:options_for_request) { { params:, headers: } }
+  let(:params) { (options[:params] || {}).merge(client_id: 'democlient') }
+  let(:headers) { (options[:headers] || {}).merge(http_basic_auth_header) }
+
+  def http_basic_auth_header
+    client_id = 'democlient'
+    client_secret = Rails.application.credentials.clients[client_id.to_sym]
+    auth = ActionController::HttpAuthentication::Basic.encode_credentials(client_id, client_secret)
+    { 'HTTP_AUTHORIZATION' => auth }
+  end
+end
+
+RSpec.shared_examples 'an endpoint that requires client authentication' do
+  context 'without client_id param' do
+    let(:params) { nil }
+
+    it 'returns HTTP status unauthorized' do
+      call_endpoint
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'returns an access denied message' do
+      call_endpoint
+      expect(response.body.chomp).to eq('HTTP Basic: Access denied.')
+    end
+  end
+
+  context 'without HTTP basic auth header' do
+    let(:headers) { nil }
+
+    it 'returns HTTP status unauthorized' do
+      call_endpoint
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'returns an access denied message' do
+      call_endpoint
+      expect(response.body.chomp).to eq('HTTP Basic: Access denied.')
+    end
+  end
+end
+
+RSpec.describe OAuthController do
+  describe 'GET /authorize' do
     let(:state_token_encoder_service) { instance_double(StateTokenEncoderService, call: results) }
     let(:results) { StateTokenEncoderService::Response[status, body] }
 
     before do
       allow(StateTokenEncoderService).to receive(:new).and_return(state_token_encoder_service)
     end
+
+    include_context 'with an authenticated client', :get, :authorize_path
+
+    it_behaves_like 'an endpoint that requires client authentication'
 
     context 'when state token encoder service returns bad request status' do
       let(:status) { :bad_request }
