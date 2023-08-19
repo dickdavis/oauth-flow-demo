a1ea481e4c10b04ab53a9800be151af0a3e50d54
Dick Davis <dick@hey.com>
2023-08-18 20:23:05 -0500

    Update manual test script for client auth

--- script/manual_testing.rb
+++ script/manual_testing.rb
@@ -7,6 +7,7 @@ require 'uri'
 code_verifier = SecureRandom.base64(55).tr('+/', '-_').tr('=', '')
 code_challenge = Digest::SHA256.base64digest(code_verifier).tr('+/', '-_').tr('=', '')
 client_id = 'democlient'
+client_secret = Rails.application.credentials.clients[client_id.to_sym]
 
 puts <<~TEXT
   Copy down the code verifier and code challenge for referencing later in the process.
@@ -14,9 +15,14 @@ puts <<~TEXT
   code verifier: #{code_verifier}
   code challenge: #{code_challenge}
 
-  Enter the URL into your browser and then enter login credentials for the resource owner.
+  Enter the URL into your browser, enter the client id and secret when prompted, and then enter login credentials for the resource owner.
+  NOTE: An actual client would send the client id and secret via HTTP Basic authentication in the headers.
 
   http://localhost:3000/authorize?&client_id=#{client_id}&response_type=code&code_challenge=#{code_challenge}&code_challenge_method=S256
+
+  The client used by this test script has the following credentials:
+  client_id: #{client_id}
+  client_secret: #{client_secret}
 TEXT
 
 # puts 'Authorization code? -> '
