92869594e92de29232b50d22973b8ab45ff16465
Dick Davis <dick@hey.com>
2023-08-29 15:45:10 -0500

    Update token request validator service to incorporate suggestions (#24)

--- spec/factories/authorization_grants.rb
+++ spec/factories/authorization_grants.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :authorization_grant do
-    code_challenge { Digest::SHA256.base64digest('code_verifier').tr('+/', '-_').tr('=', '') }
+    code_challenge { Base64.urlsafe_encode64(Digest::SHA256.digest('code_verifier'), padding: false) }
     code_challenge_method { 'S256' }
     expires_at { 9.minutes.from_now }
     client_id { 'democlient' }
