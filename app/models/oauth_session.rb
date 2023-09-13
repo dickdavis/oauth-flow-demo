b843e6060042e0a7560d1042d25ecfad6652b158
Dick Davis <dick@hey.com>
2023-09-13 15:26:39 -0500

    [TAS-57] Implement access token model (#27)
    * Allow expired JWTs to validate at model level
    
    * Implement access token model

--- app/models/oauth_session.rb
+++ app/models/oauth_session.rb
@@ -12,6 +12,8 @@ class OAuthSession < ApplicationRecord
 
   VALID_UUID_REGEX = /[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}/i
 
+  delegate :user_id, to: :authorization_grant
+
   validates :access_token_jti, presence: true, uniqueness: true, format: { with: VALID_UUID_REGEX }
   encrypts :access_token_jti, deterministic: true
 
---
+++ spec/models/access_token_spec.rb
@@ -0,0 +1,128 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AccessToken do
+  subject(:model) { build(:access_token, oauth_session:) }
+
+  let(:oauth_session) { create(:oauth_session, authorization_grant:) }
+  let(:authorization_grant) { create(:authorization_grant, user:) }
+  let(:user) { create(:user) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:aud) }
+    it { is_expected.not_to allow_value('http://foo.bar/api').for(:aud) }
+
+    it { is_expected.to validate_presence_of(:exp) }
+    it { is_expected.not_to allow_value(5.minutes.ago.to_i).for(:exp) }
+
+    it { is_expected.to validate_presence_of(:iss) }
+    it { is_expected.not_to allow_value('http://foo.bar/').for(:iss) }
+
+    it { is_expected.to validate_presence_of(:jti) }
+    it { is_expected.not_to allow_value('does-not-map-to-oauth-session').for(:jti) }
+
+    it { is_expected.to validate_presence_of(:user_id) }
+
+    it 'adds an error if the provided `user_id` claim does not map to the original authorization grant' do
+      other_user = create(:user)
+      model.user_id = other_user.id
+      model.valid?
+      expect(model.errors).to include(:user_id)
+    end
+
+    it 'does not add any errors if all provided claims are valid' do
+      expect(model).to be_valid
+    end
+  end
+
+  describe 'callbacks' do
+    describe '#revoke_oauth_session' do
+      context 'when the `aud` claim is invalid' do
+        it 'updates the OAuthSession status to `revoked`' do
+          model.aud = ''
+          expect do
+            model.valid?
+            oauth_session.reload
+          end.to change(oauth_session, :status).from('created').to('revoked')
+        end
+      end
+
+      context 'when the `iss` claim is invalid' do
+        it 'updates the OAuthSession status to `revoked`' do
+          model.iss = ''
+          expect do
+            model.valid?
+            oauth_session.reload
+          end.to change(oauth_session, :status).from('created').to('revoked')
+        end
+      end
+
+      context 'when the `exp` claim is invalid' do
+        it 'does not update the OAuthSession status to `revoked`' do
+          model.exp = ''
+          model.valid?
+          expect(oauth_session.reload).not_to be_revoked_status
+        end
+      end
+
+      context 'when the `jti` claim is invalid' do
+        it 'does not update the OAuthSession status to `revoked`' do
+          model.jti = ''
+          model.valid?
+          expect(oauth_session.reload).not_to be_revoked_status
+        end
+      end
+
+      context 'when the `user_id` claim is invalid' do
+        it 'updates the OAuthSession status to `revoked`' do
+          model.user_id = ''
+          expect do
+            model.valid?
+            oauth_session.reload
+          end.to change(oauth_session, :status).from('created').to('revoked')
+        end
+      end
+    end
+
+    describe '#expire_oauth_session' do
+      context 'when the `exp` claim is blank' do
+        it 'updates the OAuthSession status to `expired`' do
+          model.exp = ''
+          expect do
+            model.valid?
+            oauth_session.reload
+          end.to change(oauth_session, :status).from('created').to('expired')
+        end
+      end
+
+      context 'when the `exp` claim is expired' do
+        it 'updates the OAuthSession status to `expired`' do
+          model.exp = 5.minutes.ago.to_i
+          expect do
+            model.valid?
+            oauth_session.reload
+          end.to change(oauth_session, :status).from('created').to('expired')
+        end
+      end
+
+      context 'when the `exp` is invalid and a revocable claim is also invalid' do
+        it 'does not update the OAuthSession status to `expired`' do
+          model.exp = ''
+          model.aud = ''
+          model.valid?
+          expect(oauth_session.reload).not_to be_expired_status
+        end
+
+        it 'updates the OAuthSession status to `revoked`' do
+          model.exp = ''
+          model.aud = ''
+          expect do
+            model.valid?
+            oauth_session.reload
+          end.to change(oauth_session, :status).from('created').to('revoked')
+        end
+      end
+    end
+  end
+end
