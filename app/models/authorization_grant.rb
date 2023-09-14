bffdb2686689c381df5c176c90d52c48de5f3377
Dick Davis <dick@hey.com>
2023-09-13 20:52:49 -0500

    Extract OAuthSession creation logic to model

--- app/models/authorization_grant.rb
+++ app/models/authorization_grant.rb
@@ -18,7 +18,41 @@ class AuthorizationGrant < ApplicationRecord
 
   validate :client_configuration
 
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  def create_oauth_session
+    access_token_expiration = oauth_config.access_token_expiration.minutes.from_now
+
+    access_token_jti, access_token = OAuthTokenEncoderService.call(
+      client_id:,
+      expiration: access_token_expiration,
+      optional_claims: { user_id: }
+    ).deconstruct
+
+    refresh_token_jti, refresh_token = OAuthTokenEncoderService.call(
+      client_id:,
+      expiration: oauth_config.refresh_token_expiration.minutes.from_now
+    ).deconstruct
+
+    oauth_session = oauth_sessions.new(access_token_jti:, refresh_token_jti:)
+    if oauth_session.save
+      update(redeemed: true) unless redeemed?
+      Data
+        .define(:access_token, :refresh_token, :expiration)
+        .new(access_token, refresh_token, access_token_expiration.to_i)
+    else
+      errors = oauth_session.errors.full_messages.join(', ')
+      raise OAuth::ServerError, I18n.t('oauth.server_error.oauth_session_failure', errors:)
+    end
+  rescue OAuth::InvalidTokenParamError => error
+    raise OAuth::ServerError, error.message
+  end
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+
   private
+
+  def oauth_config
+    @oauth_config ||= Rails.configuration.oauth
+  end
 
   def generate_expires_at
     self.expires_at ||= 5.minutes.from_now
@@ -27,7 +61,7 @@ class AuthorizationGrant < ApplicationRecord
   def client_configuration
     return unless client_id
 
-    client_config = Rails.configuration.oauth.clients[client_id.to_sym]
+    client_config = oauth_config.clients[client_id.to_sym]
     errors.add(:client_id, 'Provided client_id does not map to a configured client') and return if client_config.blank?
 
     return unless client_config[:redirection_uri] != client_redirection_uri
--- spec/models/authorization_grant_spec.rb
+++ spec/models/authorization_grant_spec.rb
@@ -42,4 +42,89 @@ RSpec.describe AuthorizationGrant do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to have_many(:oauth_sessions) }
   end
+
+  describe '#create_oauth_session' do
+    subject(:method_call) { authorization_grant.create_oauth_session }
+
+    let(:authorization_grant) { create(:authorization_grant) }
+
+    context 'when the token request validator service does not raise an error' do
+      it 'calls the oauth token encoder service to create both the access and refresh tokens' do
+        allow(OAuthTokenEncoderService).to receive(:new).and_call_original
+        method_call
+        expect(OAuthTokenEncoderService).to have_received(:new).exactly(2).times
+      end
+
+      it 'creates an OAuthSession record' do
+        expect { method_call }.to change(OAuthSession, :count).by(1)
+      end
+
+      it 'returns a valid access token' do
+        results = method_call
+        expect(results.access_token).to match(/\A[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+\z/)
+      end
+
+      it 'saves the access_token_jti in the OAuthSession' do
+        results = method_call
+        token = JsonWebToken.decode(results.access_token)
+        expect(OAuthSession.last.access_token_jti).to eq(token[:jti])
+      end
+
+      it 'returns a valid refresh token' do
+        results = method_call
+        expect(results.refresh_token).to match(/\A[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+\z/)
+      end
+
+      it 'saves the refresh_token_jti in the OAuthSession' do
+        results = method_call
+        token = JsonWebToken.decode(results.refresh_token)
+        expect(OAuthSession.last.refresh_token_jti).to eq(token[:jti])
+      end
+
+      it 'returns the access token expiration' do
+        results = method_call
+        token = JsonWebToken.decode(results.access_token)
+        expect(token[:exp]).to eq(results.expiration)
+      end
+
+      context 'when the authorization grant has not been redeemed' do
+        it 'updates the redeemed attribute' do
+          expect { method_call }.to change(authorization_grant, :redeemed).from(false).to(true)
+        end
+      end
+
+      context 'when the authorization grant has already been redeemed' do
+        let(:authorization_grant) { create(:authorization_grant, redeemed: true) }
+
+        it 'does not update the redeemed attribute' do
+          expect { method_call }.not_to change(authorization_grant, :redeemed)
+        end
+      end
+    end
+
+    context 'when the oauth token encoder service raises the invalid request error' do
+      before do
+        allow(OAuthTokenEncoderService).to receive(:new).and_raise(OAuth::InvalidTokenParamError, 'foobar')
+      end
+
+      it 'raises an OAuth::ServerError' do
+        expect { method_call }.to raise_error(OAuth::ServerError, 'foobar')
+      end
+    end
+
+    context 'when the oauth session fails to create' do
+      let(:oauth_session_double) { build(:oauth_session, authorization_grant:, access_token_jti: nil) }
+
+      before do
+        allow(OAuthSession).to receive(:new).and_return(oauth_session_double)
+      end
+
+      it 'raises an OAuth::ServerError' do
+        expect { method_call }.to raise_error(
+          OAuth::ServerError,
+          "Failed to create OAuthSession. Errors: Access token jti can't be blank, Access token jti is invalid"
+        )
+      end
+    end
+  end
 end
