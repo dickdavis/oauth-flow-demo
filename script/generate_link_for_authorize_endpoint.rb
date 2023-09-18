3d749dffd5139595d2d75303008ba029a012bdba
Dick Davis <dick@hey.com>
2023-09-18 17:22:47 -0500

    Refactor manual testing for preferring Postman

--- script/generate_link_for_authorize_endpoint.rb
+++ script/generate_link_for_authorize_endpoint.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require 'digest'
-require 'net/http'
-require 'uri'
 
 code_verifier = SecureRandom.base64(55).tr('+/', '-_').tr('=', '')
 code_challenge = Digest::SHA256.base64digest(code_verifier).tr('+/', '-_').tr('=', '')
@@ -10,7 +8,7 @@ client_id = 'democlient'
 client_secret = Rails.application.credentials.clients[client_id.to_sym]
 
 puts <<~TEXT
-  Copy down the code verifier and code challenge for referencing later in the process.
+  Copy down the code verifier and code challenge for referencing later in the process when using Postman to send requests.
 
   code verifier: #{code_verifier}
   code challenge: #{code_challenge}
@@ -23,37 +21,4 @@ puts <<~TEXT
   The client used by this test script has the following credentials:
   client_id: #{client_id}
   client_secret: #{client_secret}
-
-TEXT
-
-puts 'Authorization code? -> '
-authorization_code = gets.chomp
-
-uri = URI.parse('http://localhost:3000/oauth/token')
-request = Net::HTTP::Post.new(uri)
-request.basic_auth(client_id, client_secret)
-request.set_form_data(
-  'code' => authorization_code,
-  'code_verifier' => code_verifier,
-  'grant_type' => 'authorization_code'
-)
-
-response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-  http.request(request)
-end
-
-parsed_response = JSON.parse(response.body)
-
-puts <<~TEXT
-
-  Access token:
-  #{parsed_response['access_token']}
-
-  Refresh token:
-  #{parsed_response['refresh_token']}
-
-  Token type: #{parsed_response['token_type']}
-  Expires in: #{parsed_response['expires_in']}
-
-  #{response.body}
 TEXT
