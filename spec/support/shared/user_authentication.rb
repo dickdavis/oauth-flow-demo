0576338ea7f0387eee8a1ccf22973f572193f48b
Dick Davis <dick@hey.com>
2024-06-08 01:58:47 -0500

    Refactor user authentication shared examples

--- spec/support/shared/user_authentication.rb
+++ spec/support/shared/user_authentication.rb
@@ -6,6 +6,10 @@ RSpec.shared_examples 'an endpoint that requires user authentication' do
       delete sign_out_path
     end
 
+    after do
+      sign_in(user)
+    end
+
     it 'redirects to the sign_in page' do
       call_endpoint
       expect(response).to redirect_to(sign_in_path)
