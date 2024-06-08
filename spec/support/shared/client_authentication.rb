9ea2d78c24ee17c7bc14a1aab5399751f5240490
Dick Davis <dick@hey.com>
2024-06-08 01:27:43 -0500

    Refactor client authentication shared examples

--- spec/support/shared/client_authentication.rb
+++ spec/support/shared/client_authentication.rb
@@ -5,10 +5,9 @@ RSpec.shared_context 'with an authenticated client' do |method, path|
 
   let(:url) { send(path) }
   let(:options_for_request) { { params: shared_context_params, headers: shared_context_headers } }
-  let(:shared_context_params) { params.reverse_merge!(client_id: 'democlient') }
-  let(:shared_context_headers) { headers.reverse_merge!(http_basic_auth_header) }
-
-  def http_basic_auth_header
+  let(:shared_context_params) { (try(:params) || {}).reverse_merge!(client_id: 'democlient') }
+  let(:shared_context_headers) { (try(:headers) || {}).reverse_merge!(http_basic_auth_header) }
+  let(:http_basic_auth_header) do
     client_id = 'democlient'
     client_secret = Rails.application.credentials.clients[client_id.to_sym]
     auth = ActionController::HttpAuthentication::Basic.encode_credentials(client_id, client_secret)
@@ -17,31 +16,25 @@ RSpec.shared_context 'with an authenticated client' do |method, path|
 end
 
 RSpec.shared_examples 'an endpoint that requires client authentication' do
+  shared_examples 'returns HTTP status unauthorized and access denied message' do
+    it 'returns HTTP status unauthorized and access denied message' do
+      call_endpoint
+      aggregate_failures do
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body.chomp).to eq('HTTP Basic: Access denied.')
+      end
+    end
+  end
+
   context 'with client_id param that does not match client id in header' do
     let(:shared_context_params) { super().merge(client_id: 'negativetestclient') }
 
-    it 'returns HTTP status unauthorized' do
-      call_endpoint
-      expect(response).to have_http_status(:unauthorized)
-    end
-
-    it 'returns an access denied message' do
-      call_endpoint
-      expect(response.body.chomp).to eq('HTTP Basic: Access denied.')
-    end
+    include_examples 'returns HTTP status unauthorized and access denied message'
   end
 
   context 'without HTTP basic auth header' do
     let(:shared_context_headers) { super().except('HTTP_AUTHORIZATION') }
 
-    it 'returns HTTP status unauthorized' do
-      call_endpoint
-      expect(response).to have_http_status(:unauthorized)
-    end
-
-    it 'returns an access denied message' do
-      call_endpoint
-      expect(response.body.chomp).to eq('HTTP Basic: Access denied.')
-    end
+    include_examples 'returns HTTP status unauthorized and access denied message'
   end
 end
