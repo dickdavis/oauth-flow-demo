369109919d279974e7e083a69a47067c4a8ce7f2
Emily Nguyen <emilyn0614@gmail.com>
2024-07-12 15:22:13 -0500

    Restrict api key generation to only confidential client type

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,8 @@ client = OAuth::Client.find_or_initialize_by(name: 'Sample Client') do |c|
   c.redirect_uri = 'http://localhost:3000/'
 end
 client.save if client.new_record?
+
+OAuth::Client.find_or_initialize_by(name: 'Public Client') do |c|
+  c.client_type = 'public'
+  c.redirect_uri = 'http://localhost:3000/'
+end
