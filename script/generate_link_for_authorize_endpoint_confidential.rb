5666a8ee1a39e0399c27215c2f5ecda7fe157e2a
Emily Nguyen <emilyn0614@gmail.com>
2024-07-15 15:58:22 -0500

    Remove unused variables and script logic

--- script/generate_link_for_authorize_endpoint_confidential.rb
+++ script/generate_link_for_authorize_endpoint_confidential.rb
@@ -2,17 +2,12 @@
 
 require 'digest'
 
-code_verifier = SecureRandom.base64(55).tr('+/', '-_').tr('=', '')
-code_challenge = Digest::SHA256.base64digest(code_verifier).tr('+/', '-_').tr('=', '')
 client = OAuth::Client.where(client_type: 'confidential').first
 client_id = client.id
 client_secret = client.api_key
 
 puts <<~TEXT
   Copy down the code verifier and code challenge for referencing later in the process when using Postman to send requests.
-
-  code verifier: #{code_verifier}
-  code challenge: #{code_challenge}
 
   Enter the URL into your browser, enter the client id and secret when prompted, and then enter login credentials for the resource owner.
   NOTE: An actual client would send the client id and secret via HTTP Basic authentication in the headers.
