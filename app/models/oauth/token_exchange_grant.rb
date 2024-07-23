c93e972f7f187281e48be5a814f61a73d2ff914f
Emily Nguyen <emilyn0614@gmail.com>
2024-07-23 13:39:59 -0500

    Fix class migration name and association

--- app/models/oauth/token_exchange_grant.rb
+++ app/models/oauth/token_exchange_grant.rb
@@ -8,10 +8,5 @@ module OAuth
 
     belongs_to :user
     belongs_to :oauth_client, class_name: 'OAuth::Client'
-    has_many :oauth_sessions,
-             class_name: 'OAuth::Session',
-             foreign_key: :oauth_authorization_grant_id,
-             inverse_of: :oauth_authorization_grant,
-             dependent: :destroy
   end
 end
